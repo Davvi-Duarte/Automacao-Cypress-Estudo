d0ab281a953b3e68f06d6f5225edf0b9238892b1
Davvi Duarte Rodrigues <49027886+Davvi-Duarte@users.noreply.github.com>
2025-04-23 22:46:03 -0300

    Update login.spec.cy.js

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -9,7 +9,8 @@ describe('Orange HRM Tests - Login', () => {
     loginErrorMessage: ".oxd-alert",
     dashboardGrid: ".orangehrm-dashboard-grid",
     inputErrorUsernameRequire: ":nth-child(2) > .oxd-input-group > .oxd-text",
-    inputErrorPasswordRequire: ":nth-child(3) > .oxd-input-group > .oxd-text",
+    inputErrorPasswordRequire: ":nth-child(3) > .oxd-input-group > .oxd-text"
+  
   }
 
   it('Login - Sucess', () => {
